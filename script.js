51207a2bb9cba3698aa84cc7db23787ad371427e
Tamires Cavalcante <69002877+tamiresCavalcante@users.noreply.github.com>
2020-12-20 20:17:49 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+if(window.SimpleSlide){
+
+    new SimpleSlide({
+        slide:"quote",
+        auto: true,
+        time: 5000,
+        nav:true
+});
+
+}
+//var footer = document.querySelector(".footer_redes");
+
+//footer.innerHTML = "<p>teste</p>";
+
+if(window.SimpleAnime) {
+    new SimpleAnime(); 
+}
